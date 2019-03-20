62c7b51ed30e8f97441ea55556c753089eb1f9b2
Arthur van Hoff <avh@artfahrt.com>
2019-03-19 17:43:42 -0700

    add carribean and grand canyon

--- src/chart_list_update.py
+++ src/chart_list_update.py
@@ -28,11 +28,41 @@ for tr in html.xpath(".//div[@id='sectional']/table/tbody/tr"):
     #print(sec)
     sec_list.set(name, sec)
 
+for tr in html.xpath(".//div[@id='caribbean']/table/tbody/tr"):
+    name = ''.join(tr[0].text)
+    text = ''.join(tr[1].text).split(" - ")
+    href = tr[1].find(".//a").attrib["href"]
+    sec = {
+        'name': name,
+        'type': 'sec',
+        'current': int(text[0]),
+        'date': text[1],
+        'href': href,
+        'path': "data/charts/source/" + os.path.splitext(os.path.basename(href))[0]
+    }
+    #print(sec)
+    sec_list.set(name, sec)
+
 tac_list = settings.db.hash_table("tac_list")
 tac_list.clear()
 for tr in html.xpath(".//div[@id='terminalArea']/table/tbody/tr"):
     name = ''.join(tr[0].text)
     text = ''.join(tr[1].text).split(" – ")
+    href = tr[1].find(".//a").attrib["href"]
+    tac = {
+        'name': name,
+        'type': 'tac',
+        'current': int(text[0]),
+        'date': text[1],
+        'href': href,
+        'path': "data/charts/source/" + os.path.splitext(os.path.basename(href))[0]
+    }
+    #print(tac)
+    tac_list.set(name, tac)
+
+for tr in html.xpath(".//div[@id='grandCanyon']/table/tbody/tr"):
+    name = ''.join(tr[0].text)
+    text = ''.join(tr[1].text).split(" - ")
     href = tr[1].find(".//a").attrib["href"]
     tac = {
         'name': name,
