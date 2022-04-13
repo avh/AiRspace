db707dd58884cd8dab19d5b1366c4234c2554647
Arthur van Hoff <avh@artfahrt.com>
2022-04-12 20:09:57 -0700

    IFR chart list update, IFR chart download

--- src/chart_data_download.py
+++ src/chart_data_download.py
@@ -1,29 +1,52 @@
 # (c)2018-2020, Artfahrt Inc, Arthur van Hoff
 
-import os, zipfile, time
+import os, zipfile, time, multiprocessing
 import settings, util
 
-def download_chart(chart):
-    path = chart['path']
-    if os.path.exists(path):
-        return
+max_download_workers = 8
 
-    zip_path = util.download_file(chart['href'])
-    if zip_path is None:
-        return
+def download_chart(q):
+    while True:
+        task = q.get()
+        if task[0] == 'done':
+            break
 
-    try:
-        with zipfile.ZipFile(zip_path) as zip:
-            zip.extractall(settings.tmp_dir)
-            print("installing", chart['name'])
-            os.makedirs(path, exist_ok=True)
-            for name in zip.namelist():
-                os.rename(os.path.join(settings.tmp_dir, name), os.path.join(path, name))
-    except zipfile.BadZipFile:
-        print("%s: bad zipfile" % (zip_path))
-    finally:
-        #os.remove(zip_path)
-        pass
+        chart = task[1]
+        path = chart['path']
+        if os.path.exists(path):
+            continue
+
+        zip_path = util.download_file(chart['href'])
+        if zip_path is None:
+            continue
+
+        try:
+            with zipfile.ZipFile(zip_path) as zip:
+                zip.extractall(settings.tmp_dir)
+                print("installing", chart['name'])
+                os.makedirs(path, exist_ok=True)
+                for name in zip.namelist():
+                    os.rename(os.path.join(settings.tmp_dir, name), os.path.join(path, name))
+        except zipfile.BadZipFile:
+            print("%s: bad zipfile" % (zip_path))
+        finally:
+            #os.remove(zip_path)
+            pass
+
+def download_charts(list_name, max_workers=max_download_workers):
+    chart_list = settings.db.hash_table(list_name)
+    max_workers = min(chart_list.count(), max_workers)
+
+    q = ctx.Queue(max_workers*2)
+    workers = [ctx.Process(target=download_chart, args=(q,)) for _ in range(max_workers)]
+    for w in workers:
+        w.start()
+    for chart in chart_list.all():
+        q.put(('chart', chart))
+    for _ in range(max_workers):
+        q.put(('done', ))
+    for w in workers:
+        w.join()
 
 
 #
@@ -31,21 +54,13 @@ def download_chart(chart):
 #
 
 if __name__ == '__main__':
+    ctx = multiprocessing.get_context('spawn')
     tm = time.time()
-    sec_list = settings.db.hash_table("sec_list")
-    for chart in sec_list.all():
-        download_chart(chart)
-
-    tac_list = settings.db.hash_table("tac_list")
-    for chart in tac_list.all():
-        download_chart(chart)
-
-    heli_list = settings.db.hash_table("heli_list")
-    for chart in heli_list.all():
-        download_chart(chart)
-
-    plan_list = settings.db.hash_table("plan_list")
-    for chart in plan_list.all():
-        download_chart(chart)
-
+    download_charts("sec_list")
+    download_charts("tac_list")
+    download_charts("heli_list")
+    download_charts("plan_list")
+    download_charts("ifr_low_list")
+    download_charts("ifr_high_list")
+    download_charts("ifr_area_list")
     print(f"done in {util.time_str(tm)}")
--- src/chart_list_update.py
+++ src/chart_list_update.py
@@ -1,6 +1,6 @@
 # (c)2018-2022, Artfhart Inc, Arthur van Hoff
 
-import os, requests, lxml.etree, dateutil, time
+import os, requests, lxml.html, lxml.etree, dateutil, time
 import settings, util
 
 #
@@ -22,28 +22,57 @@ def update_chart_list(html, tag, kind, table, col=1):
 
 if __name__ == '__main__':
     tm = time.time()
-    req = requests.get(url=settings.faa_chart_url)
-    html = lxml.etree.HTML(req.text)
 
-    sec_list = settings.db.hash_table('sec_list')
-    sec_list.clear()
-    update_chart_list(html, 'sectional', 'sec', sec_list)
-    update_chart_list(html, 'carribean', 'sec', sec_list)
+    if True:
+        req = requests.get(url=settings.faa_vfr_chart_url)
+        html = lxml.etree.HTML(req.text)
 
-    tac_list = settings.db.hash_table("tac_list")
-    tac_list.clear()
-    update_chart_list(html, 'terminalArea', 'tac', tac_list)
-    update_chart_list(html, 'grandCanyon', 'tac', tac_list)
+        sec_list = settings.db.hash_table("sec_list")
+        sec_list.clear()
+        update_chart_list(html, 'sectional', 'sec', sec_list)
+        update_chart_list(html, 'caribbean', 'sec', sec_list)
 
-    hel_list = settings.db.hash_table("hel_list")
-    hel_list.clear()
-    update_chart_list(html, 'helicopter', 'hel', hel_list)
+        tac_list = settings.db.hash_table("tac_list")
+        tac_list.clear()
+        update_chart_list(html, 'terminalArea', 'tac', tac_list)
+        update_chart_list(html, 'grandCanyon', 'tac', tac_list)
 
-    pln_list = settings.db.hash_table("pln_list")
-    update_chart_list(html, 'Planning', 'pln', pln_list)
+        hel_list = settings.db.hash_table("hel_list")
+        hel_list.clear()
+        update_chart_list(html, 'helicopter', 'hel', hel_list)
 
-    print(f"{sec_list.count()} sectional charts")
-    print(f"{tac_list.count()} terminal area charts")
-    print(f"{hel_list.count()} helicopter charts")
-    print(f"{pln_list.count()} planning charts")
+        pln_list = settings.db.hash_table("pln_list")
+        update_chart_list(html, 'Planning', 'pln', pln_list)
+
+
+        print(f"{sec_list.count():4} VFR sectional charts")
+        print(f"{tac_list.count():4} VFR terminal area charts")
+        print(f"{hel_list.count():4} VFR helicopter charts")
+        print(f"{pln_list.count():4} VFR planning charts")
+
+    if True:
+        req = requests.get(url=settings.faa_ifr_chart_url)
+        html = lxml.etree.HTML(req.text)
+
+        ifr_list = settings.db.hash_table("ifr_list")
+        ifr_list.clear()
+        update_chart_list(html, 'lowsHighsAreas', 'ifr', ifr_list)
+        ifr_low_list = settings.db.hash_table("ifr_low_list")
+        ifr_high_list = settings.db.hash_table("ifr_high_list")
+        ifr_area_list = settings.db.hash_table("ifr_area_list")
+        for s in ifr_list.all():
+            name = s['name']
+            if name.startswith("EL"):
+                ifr_low_list.set(name, s)
+            elif name.startswith("EH"):
+                ifr_high_list.set(name, s)
+            elif name.startswith("EPHI"):
+                ifr_high_list.set(name, s)
+            elif name.startswith("Area"):
+                ifr_area_list.set(name, s)
+
+        print(f"{ifr_low_list.count():4} IFR low charts")
+        print(f"{ifr_high_list.count():4} IFR high charts")
+        print(f"{ifr_area_list.count():4} IFR area charts")
+
     print(f"done in {util.time_str(tm)}")
